ecfd3321a2a02de6bc26d9b303b654ad641ad188
Pavel <triodjangopiter@yandex.ru>
2017-11-12 22:13:35 +0300

    add readme

--- src/main/java/ru/javavision/Application.java
+++ src/main/java/ru/javavision/Application.java
@@ -5,33 +5,10 @@ package ru.javavision;
  * Created : 02/11/2017.
  */
 public class Application {
-//    public static void main(String[] args) {
-//
-//        PhoneDAO phoneDAO = null;
-//
-//        try {
-////            phoneDAO = new PhoneDAOImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones_magazine");
-////
-////            StatisticRepository dao =  new StatisticRepositoryImpl("postgres", "1", "jdbc:postgresql://localhost:5432/phones_magazine");
-////
-////            final PhoneModel model = new PhoneModel();
-////            model.setName("samsung");
-////            List<PhoneModel> models = new ArrayList<>();
-////            models.add(model);
-////            dao.getStat(
-////                    models,
-////                    new Statistic.TimeRange(
-////                            new Timestamp(System.currentTimeMillis() - 31536000000L),
-////                            new Timestamp(System.currentTimeMillis())),
-////                    Comparator.comparing(Statistic::getRevenue)
-////                    ).forEach(System.out::println);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        } finally {
-//            assert phoneDAO != null;
-//            phoneDAO.closeConnection();
-//        }
-//    }
-
+    /*
+      See branch for learning:
+      lesson/01.connection
+      lesson/02.dao
+      lesson/03.repository
+     */
 }
--- src/main/java/ru/javavision/service/PhoneService.java
+++
@@ -1,9 +0,0 @@
-package ru.javavision.service;
-
-import ru.javavision.model.Statistic;
-
-import java.util.SortedSet;
-
-public interface PhoneService {
-    SortedSet<Statistic> getStatLastYear();
-}
