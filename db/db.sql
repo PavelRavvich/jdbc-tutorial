19763a279cf230798193d85760945abc8f1b3cb0
Pavel <triodjangopiter@yandex.ru>
2017-11-12 21:29:00 +0300

    complete repository lesson

--- db/db.sql
+++ db/db.sql
@@ -1,14 +1,15 @@
 CREATE DATABASE phones_magazine ENCODING 'UTF-8';
 
+--Создаем таблицу ролей для пользователей системы.
 CREATE TABLE IF NOT EXISTS roles (
   id   SERIAL PRIMARY KEY,
   role VARCHAR(5) NOT NULL
 );
-
+--Заполняем роли.
 INSERT INTO roles (id, role) VALUES (DEFAULT, 'admin');
 INSERT INTO roles (id, role) VALUES (DEFAULT, 'user');
 
-
+--Создаем таблицу пользователей.
 CREATE TABLE IF NOT EXISTS users (
   id       SERIAL PRIMARY KEY,
   login    VARCHAR(10) UNIQUE NOT NULL,
@@ -24,32 +25,24 @@ DELETE FROM users WHERE id = (?) AND login = (?) AND password = (?) RETURNING id
 --Обновить пользователя
 UPDATE users SET password = (?) WHERE id = (?) RETURNING id;
 
+--Вставляем новых пользователей в таблицу.
 INSERT INTO users (id, login, password, role) VALUES (DEFAULT, 'admin', '123', 1);
-
 INSERT INTO users (id, login, password, role) VALUES (DEFAULT, 'user', '1234', 2);
 
---Created 1 time.
+--Создаем таблицу моделей.
 CREATE TABLE IF NOT EXISTS phone_models (
   id   SERIAL PRIMARY KEY,
   name VARCHAR(15) UNIQUE NOT NULL
 );
 
-INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'samsung')
-RETURNING id;
+--Вставляем модели в таблицу.
+INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'samsung')RETURNING id;
+INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'iphone')RETURNING id;
+INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'xaomi')RETURNING id;
+INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'digma')RETURNING id;
 
-INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'iphone')
-RETURNING id;
 
-INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'xaomi')
-RETURNING id;
-
-INSERT INTO phone_models (id, name) VALUES (DEFAULT, 'digma')
-RETURNING id;
-
-SELECT m.name
-FROM phone_models AS m;
-
---Created 2 time.
+--Создаем таблицу для записей информации о продажах.
 CREATE TABLE IF NOT EXISTS phones_sale (
   id       SERIAL PRIMARY KEY,
   model_id INTEGER   NOT NULL,
@@ -60,20 +53,12 @@ CREATE TABLE IF NOT EXISTS phones_sale (
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
-INSERT INTO phones_sale (id, model_id, price, date, user_id)
-VALUES (DEFAULT, 1, 58000, now(), 2);
-
-INSERT INTO phones_sale (id, model_id, price, date, user_id)
-VALUES (DEFAULT, 2, 90000, now(), 2);
-
-INSERT INTO phones_sale (id, model_id, price, date, user_id)
-VALUES (DEFAULT, 3, 30000, now(), 2);
-
-INSERT INTO phones_sale (id, model_id, price, date, user_id)
-VALUES (DEFAULT, 1, 64000, now(), 1);
-
-INSERT INTO phones_sale (id, model_id, price, date, user_id)
-VALUES (DEFAULT, 3, 35000, now(), 1);
+--Вставляем данные о продажах.
+INSERT INTO phones_sale (id, model_id, price, date, user_id) VALUES (DEFAULT, 1, 58000, now(), 2);
+INSERT INTO phones_sale (id, model_id, price, date, user_id) VALUES (DEFAULT, 2, 90000, now(), 2);
+INSERT INTO phones_sale (id, model_id, price, date, user_id) VALUES (DEFAULT, 3, 30000, now(), 2);
+INSERT INTO phones_sale (id, model_id, price, date, user_id) VALUES (DEFAULT, 1, 64000, now(), 1);
+INSERT INTO phones_sale (id, model_id, price, date, user_id) VALUES (DEFAULT, 3, 35000, now(), 1);
 
 --Выборка общей суммы продаж по промежутку времени.
 SELECT sum(p.price)
@@ -81,10 +66,7 @@ FROM phones_sale AS p
 WHERE p.date >= '2017-10-04 00:16:22.123102' AND
       p.date <= '2017-12-30 23:21:31.59098';
 
---Получение суммы выручки за модель за промежуток времени
-SELECT *
-FROM phone_models;
-
+--Получение суммы выручки по модели за промежуток времени.
 SELECT sum(p.price)
 FROM phones_sale AS p
   LEFT JOIN phone_models AS m ON p.model_id = m.id
